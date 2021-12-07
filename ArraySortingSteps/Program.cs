434d85f2673ed9c737acc5ed972ca31393b18e39
Lilit Khachatryan <lilit.a.khachatryan@praemium.com>
2021-12-07 23:38:08 +0400

    Add project files.

---
+++ ArraySortingSteps/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArraySortingSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MergeSort sort = new MergeSort();
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            Console.WriteLine("Given Array");
+            MergeSort.PrintArray(arr);
+            sort.Sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nSorted array");
+            MergeSort.PrintArray(arr);
+
+            var count = sort.CountSteps();
+            Console.WriteLine($"Number of steps taken to sort an array: {count}");
+        }
+
+
+    }
+
+
+}
